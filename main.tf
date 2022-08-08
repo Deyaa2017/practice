b9f0a67caded3d392872d52e9d4afaf84fff10a3
Deyaa2017 <110706115+Deyaa2017@users.noreply.github.com>
2022-08-08 09:22:24 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,82 @@
+variable "awsprops" {
+    
+    default = {
+    region = "us-east-1"
+    vpc = "vpc-0b56d3938f9d860fc"
+    ami = "ami-0cff7528ff583bf9a"
+    itype = "t2.small"
+    subnet = "subnet-0492c92dc213af1cd"
+    publicip = true
+    keyname = "nv"
+    secgroupname = "TestingSecurityGroupp"
+  }
+}
+
+provider "aws" {
+  region = lookup(var.awsprops, "region")
+  access_key = "AKIASJARONS2RCTELGKB"
+  secret_key = "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"
+}
+
+resource "aws_security_group" "project-iac-sg" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.awsprops, "vpc")
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_instance" "project-iac" {
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+vpc_security_group_ids = [
+    aws_security_group.project-iac-sg.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 8
+    volume_type = "gp2"
+  }
+  tags = {
+    Name ="TerraFormLaunch"
+    Environment = "DEV"
+    OS = "UBUNTU"
+    Managed = "IAC"
+  }
+
+  depends_on = [ aws_security_group.project-iac-sg ]
+}
+
+
+output "ec2instance" {
+  value = aws_instance.project-iac.public_ip
+}
