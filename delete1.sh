0dd1d52d8ed2080c83fd93362493895ce293f1ff
Deyaa2017 <110706115+Deyaa2017@users.noreply.github.com>
2022-08-21 12:21:48 +0530

    Update delete1.sh

--- delete1.sh
+++ delete1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+export AWS_ACCESS_KEY_ID=AKIAWJQD4U5F6OFTFU6J
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-east-1
 
 
 aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[].Instances[].InstanceId' --filters "Name=tag:name,Values=test" --output text)
