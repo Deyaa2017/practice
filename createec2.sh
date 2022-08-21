6a1210e0b026aa9d83fdf5ef790d4a1ffa846536
Deyaa2017 <110706115+Deyaa2017@users.noreply.github.com>
2022-08-21 11:41:16 +0530

    Update createec2.sh

--- createec2.sh
+++ createec2.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-echo "enter the amiid"
 
-read amiid
 
 export aws_access_key_id=AKIAWJQD4U5F6OFTFU6J
 export aws_secret_key_id=XGkXDXnAVT+FekySAD0XRMcgCcpc7HhkF+zXF2to
 export aws_default_region=us-east-1
-export aws_default_format='table'
+export default_format='table'
 
 aws ec2 run-instances --image-id $amiid --count 1 --instance-type t2.micro --key-name nikon --security-group-ids sg-01f603249e9bcce9b --subnet-id subnet-046a5e24a31e6f8b8
 
