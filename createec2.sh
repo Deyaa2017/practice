9c4ffbfe17fceebaf7685306c1b957adb72ead6b
Deyaa2017 <110706115+Deyaa2017@users.noreply.github.com>
2022-08-21 11:46:00 +0530

    Update createec2.sh

--- createec2.sh
+++ createec2.sh
@@ -2,10 +2,10 @@
 
 
 
-export aws_access_key_id=AKIAWJQD4U5F6OFTFU6J
-export aws_secret_key_id=XGkXDXnAVT+FekySAD0XRMcgCcpc7HhkF+zXF2to
-export aws_default_region=us-east-1
-export default_format='table'
+export AWS_ACCESS_KEY_ID=AKIAWJQD4U5F6OFTFU6J
+export AWS_SECRET_KEY_ID=XGkXDXnAVT+FekySAD0XRMcgCcpc7HhkF+zXF2to
+export AWS_DEFAULT_REGION=us-east-1
+
 
 aws ec2 run-instances --image-id $amiid --count 1 --instance-type t2.micro --key-name nikon --security-group-ids sg-01f603249e9bcce9b --subnet-id subnet-046a5e24a31e6f8b8
 
