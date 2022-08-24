f0e2cf2e6cc8aeddff0cda61f6a932c70783b85b
Deyaa2017 <110706115+Deyaa2017@users.noreply.github.com>
2022-08-24 09:47:35 +0530

    Update bucket.sh

--- bucket.sh
+++ bucket.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID=AKIAWJQD4U5F6OFTFU6J
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
 export AWS_DEFAULT_REGION=us-east-1
 
 aws s3api  create-bucket --bucket $bucketname --region us-east-1 --endpoint-url https://s3.us-east-1.amazonaws.com
